750752c16a0190b2fd9671bae40858bf21a3322f
ffreemt <yucongo+fmt@gmail.com>
2019-07-28 16:18:02 +0800

    minor update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 name = """youdao-tr-free"""
 # description = ' '.join(name.split('-'))
-description = name.replace('-tr-', 'translate for')
+description = name.replace('-tr-', ' translate for ')
 dir_name, = find_packages()
 
 version, = re.findall(r"\n__version__\W*=\W*'([^']+)'", open(Path(__file__).parent / f'{dir_name}/__init__.py').read())
--- youdao_tr/__init__.py
+++ youdao_tr/__init__.py
@@ -3,6 +3,6 @@ from .youdao_tr import youdao_tr
 
 # __version__ = '0.0.1'
 # __date__ = '2019.7.23'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __date__ = '2019.7.28'
 VERSION = __version__
